cf2bb60e6bedbe7fc914b89cd65aff61324ea343
shahmaran0207 <shahmaran0207@naver.com>
2024-08-15 13:30:09 +0900

    데이터베이스 변경

--- src/main/java/com/travel/travel/Config/SecurityConfig.java
+++ src/main/java/com/travel/travel/Config/SecurityConfig.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -21,11 +21,31 @@ public class SecurityConfig {
     @Autowired
     private MemberService memberService;
 
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/Member/Login", "/Member/Login/error", "/Member/Logout", "/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/Member/Login")
+                        .defaultSuccessUrl("/")
+                        .usernameParameter("email")
+                        .failureUrl("/Member/Login/error")
+                )
+                .logout(logout -> logout
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/Member/Logout"))
+                        .logoutSuccessUrl("/")
+                );
+
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
